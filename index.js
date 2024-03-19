99b991b3bda759ad274a503f30bb48da91ba51d0
JACKA <jackarcisse@gmail.com>
2024-03-19 18:24:05 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function appendCat(name) {
+    return [...cats, name];
+}
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+ // index.js
+
+// Define the destructivelyRemoveFirstCat function
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+function removeFirstCat() {
+    return cats.slice(1);
+}
